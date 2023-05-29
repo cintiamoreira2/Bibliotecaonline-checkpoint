836a16e9ad78fe1797155be25a157ad5d3b5cb33
Cintia Moreira <Cintia1350@gmail.com>
2023-05-29 20:42:20 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
-let novosCards = document.querySelector('.novosCards')
-
-let cards = document.querySelector('.cards')
+/* let cards = document.querySelector('.novosCards')
 
 let titulo = document.querySelector('.titulo')
 
@@ -10,15 +8,70 @@ let autor = document.querySelector('.autor')
 
 let textArea = document.querySelector('#textArea')
 
-let enviar = document.querySelector('.enviar')
+let enviar = document.getElementById('enviar')
 
-enviar.addEventListener('click', function(e){
-    e.preventDefault()
-    novosCards.innerHTML += `<div id="cards">
+enviar.addEventListener('click', function(){
+
+    //if(enviar !== 0 && enviar !== null) {}
+
+
+cards.innerHTML += `<div id="cards">
 <h2>${titulo.value}</h2>
 <img src="${imagem.value}" class="imgStilos">
 <h3>${autor.value}</h3>
 <p>${textArea.value}</p>
-</div>`
-})
+</div>`})
+ */
+
+// recebe elemento formulario
+const inserir = document.querySelector('#inserir');
+
+// recebe container onde serao adicionados os cards
+const novosCards = document.querySelector('#novosCards');
+
+// lista que ira receber os objetos cards
+var cards = [];
+
+// funcao que cria o template e adiciona o card ao container
+const addCard = function (titulo, descricao, url) {
+    const template = `
+    <div class="card">
+       <img src="${url}" alt="${titulo}" >
+       <p>${titulo}</p>
+       <p>${descricao}</p>
+     </div>
+    `;
+
+    novosCards.innerHTML += template;
+}
+
+const form = document.querySelector('form'); // Adiciona a referência ao elemento <form>
+
+// event onsubmit - ao clicar botao de add imagens
+form.onsubmit = function (event) {
+    event.preventDefault();
+    var titulo = document.querySelector('#titulo').value;
+    var descricao = document.querySelector('#descricao').value;
+    var url = document.querySelector('#url').value;
+
+    // criar o card ao clicar em submit
+    var card = {
+        titulo: titulo,
+        descricao: descricao,
+        url: url
+    };
+
+    // adicionar o card à lista cards
+    cards.push(card);
+
+    // executa funcao que add o card ao DOM
+    addCard(titulo, descricao, url);
+    
+// Limpa os campos após adicionar o card
+document.querySelector('#titulo').value = '';
+document.querySelector('#descricao').value = '';
+document.querySelector('#url').value = '';
+    
+}
+
 
